7c04f749f7d83a1aa4e3a5d2457d3ff6f141dca8
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-08-09 16:57:09 -0300

    ex 12 correto

--- exercicio12.js
+++ exercicio12.js
@@ -1,33 +1,34 @@
 
 var moedas = prompt("Digite qual a moeda você tem (Euro, Dolar ou Real):") 
-var cambio = prompt("Digite para qual você gostaria de converter (Euro, Dolar ou Euro ):  ")
+var cambio = prompt("Digite para qual você gostaria de converter (Euro, Dolar ou Real ):  ")
 var possui = parseFloat(prompt("Digite a quantidade de dinheirinhos que você tem para o cambio:"))
 
-var real = 1 
-var dolar = 1
-var euro = 1
-var realParaDolar = real * 5.51
-var realParaEuro = real * 6.02 
-var dolarParaReal = real * 0.18
-var euroParaReal = real * 0.17
-var euroParaDolar = euro * 1.09
-var dolarParaEuro = dolar * 0.92 
+ 
+var realParaDolar = possui * 5.51
+var realParaEuro = possui * 6.02 
+var dolarParaReal = possui * 0.18
+var euroParaReal = possui * 6.02
+var euroParaDolar = possui * 1.09
+var dolarParaEuro = possui * 0.92 
 
 if (moedas == 'real' && cambio == 'dolar'){
-    alert(`O valor revertido de reais para dolar será de ${realParaDolar.toFixed(2)}`)
+    alert(`O valor revertido de reais para dolar será de ${realParaDolar}`)
 }
 else if (moedas == 'real' && cambio == 'euro'){
-    alert(`O valor revertido de reais para euros será de ${realParaEuro.toFixed(2)}`)
+    alert(`O valor revertido de reais para euros será de ${realParaEuro}`)
 }
 else if (moedas == 'dolar' && cambio == 'real'){
-    alert(`O valor revertido de dolares para reais será de ${dolarParaReal.toFixed(2)}`)
+    alert(`O valor revertido de dolares para reais será de ${dolarParaReal}`)
 }
 else if (moedas == 'euro' && cambio == 'real'){
-    alert(`O valor revertido de euros para reais será de ${euroParaReal.toFixed(2)}`)
+    alert(`O valor revertido de euros para reais será de ${euroParaReal}`)
 }
 else if (moedas == 'euro' && cambio == 'dolar'){
-    alert(`O valor revertido de euros para dolares será de ${euroParaDolar.toFixed(2)}`)
+    alert(`O valor revertido de euros para dolares será de ${euroParaDolar}`)
 }
 else if (moedas == 'dolar' && cambio == 'euro'){
-    alert(`O valor revertido de dolares para euros será de ${dolarParaEuro.toFixed(2)}`)
+    alert(`O valor revertido de dolares para euros será de ${dolarParaEuro}`)
 }
+
+
+//dolares para euros será de 21.7391304347826
