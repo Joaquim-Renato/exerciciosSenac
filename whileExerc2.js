2c1bb0868c1234d624e1f8951f3bc4ff9c5d05cf
JOAQUIM RENATO ALVES DE SOUZA <joaquim.rasouza@senacsp.edu.br>
2024-08-21 15:15:41 -0300

    subindo ex2 usando While

---
+++ whileExerc2.js
@@ -0,0 +1,12 @@
+var cels = 0;
+var fahr = 0;
+
+
+while (cels <= 100){
+
+var conversao = fahr = (cels * 1.8) + 32; 
+
+    document.write(`Quando a temperatura em Graus Celsius for ${cels}C° a temperatura em Fahrenheit será ${fahr}F° <br>`)
+    cels+=10
+
+}
