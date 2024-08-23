67b5c1fadf187afc6219642b5a63a4ab4b043169
JOAQUIM RENATO ALVES DE SOUZA <joaquim.rasouza@senacsp.edu.br>
2024-08-23 16:46:52 -0300

    ex 19

---
+++ exerciciosCombinados.js/exLimiteSoma.js
@@ -0,0 +1,21 @@
+var numeros = parseFloat(0);
+var somaDosNumeros = parseFloat(0);
+var totalNumeros = parseFloat(0);
+
+
+while (true){
+    var numeros = parseFloat(prompt("Digite o numero: "));
+
+    if (somaDosNumeros <= 100){
+        ++numeros;
+    }
+
+    totalNumeros++;
+    somaDosNumeros += numeros;
+
+    if (somaDosNumeros >= 100){
+        break;
+    }
+}
+
+document.write(`forma necessarios ${totalNumeros} números para alcançar uma soma maior ou igual a 100`);
